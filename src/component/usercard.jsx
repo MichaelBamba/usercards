b213e7b4b52e7d4a040e314c3879aeeffc1eda55
MichaelBamba <mjbamba2@gmail.com>
2020-03-26 09:44:06 -0400

    Expanded data to more precise info

--- src/component/usercard.jsx
+++ src/component/usercard.jsx
@@ -3,29 +3,48 @@ import "./usercard.css";
 
 class UserCard extends Component {
   state = {
-    userData: "Getting user card"
+    title: "title processing",
+    first_Name: "First Name processing",
+    last_Name: "Last Name",
+    pic: "pic",
+    email: "email",
+    phone: "phone"
   };
   async componentDidMount() {
     try {
-      let response = await fetch("https://randomuser.me/api/?results=1");
-      let data = await response.json();
-      console.log("data is ", data);
-
+      const response = await fetch(`https://randomuser.me/api/?results=1`);
+      const data = await response.json();
       this.setState({
-        userData: data.results.firstname
+        title: data.results[0].name.t,
+        first_Name: data.results[0].name.f,
+        last_Name: data.results[0].name.l,
+        pic: data.results[0].picture.large,
+        email: data.results[0].email,
+        phone: data.results[0].phone
       });
     } catch (error) {
-      this.setState({
-        userData: error.message
-      });
+      console.error("Error", error);
+      return error;
     }
   }
   render() {
-    let { userData } = this.state;
+    const { title } = this.state;
+    const { first_Name } = this.state;
+    const { last_Name } = this.state;
+    const { pic } = this.state;
+    const { email } = this.state;
+    const { phone } = this.state;
     return (
-      <div className="card">
-        <p>Welcome unwilling test subject</p>
-        <p> `{userData}`</p>
+      <div>
+        <div>
+          <p> "Unwilling test Subject" </p>
+          <img src={pic} alt="pic"></img>
+          <p>{title}</p>
+          <p>{first_Name}</p>
+          <p>{last_Name}</p>
+          <p>{email}</p>
+          <p>{phone}</p>
+        </div>
       </div>
     );
   }
